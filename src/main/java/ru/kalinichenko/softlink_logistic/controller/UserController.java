7577ca5c0de4f085f2564cd030e8010aeb3702e4
Maxim1720 <musiev17@gmail.com>
2023-06-21 00:29:41 +0400

    to deligate user registration to UserService

--- src/main/java/ru/kalinichenko/softlink_logistic/controller/UserController.java
+++ src/main/java/ru/kalinichenko/softlink_logistic/controller/UserController.java
@@ -1,9 +1,6 @@
 package ru.kalinichenko.softlink_logistic.controller;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,29 +8,26 @@ import ru.kalinichenko.softlink_logistic.dao.ConfirmationCodeRepository;
 import ru.kalinichenko.softlink_logistic.dao.UserRepository;
 import ru.kalinichenko.softlink_logistic.entity.User;
 import ru.kalinichenko.softlink_logistic.entity.auth.ConfirmationCode;
-import ru.kalinichenko.softlink_logistic.service.EmailNotificationService;
+import ru.kalinichenko.softlink_logistic.service.UserService;
 
 import java.util.Optional;
-import java.util.Random;
 
 @Controller
 @RequestMapping("/auth/register")
 public class UserController {
 
     private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
 
     private final ConfirmationCodeRepository confirmationCodeRepository;
 
-    private final EmailNotificationService notificationService;
+    private final UserService userService;
 
     public UserController(UserRepository userRepository,
-                          PasswordEncoder passwordEncoder,
-                          ConfirmationCodeRepository confirmationCodeRepository, EmailNotificationService notificationService) {
+                          ConfirmationCodeRepository confirmationCodeRepository,
+                          UserService userService) {
         this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
         this.confirmationCodeRepository = confirmationCodeRepository;
-        this.notificationService = notificationService;
+        this.userService = userService;
     }
 
     @GetMapping
@@ -45,41 +39,21 @@ public class UserController {
     @PostMapping
     public String registerUser(@ModelAttribute("user") User user, Model model, HttpSession httpSession) {
         User finalUser = user;
-        if (userRepository.exists(new Example<>() {
-            @Override
-            public User getProbe() {
-                return finalUser;
-            }
-            @Override
-            public ExampleMatcher getMatcher() {
-                return ExampleMatcher.matchingAny()
-                        .withMatcher("email", ExampleMatcher.GenericPropertyMatcher::exact)
-                        .withMatcher("username", ExampleMatcher.GenericPropertyMatcher::exact);
-            }
-        })) {
+        if (userService.exists(user)) {
             model.addAttribute("errorMessage",
                     "Пользователь с таким username или email уже существует");
             return "auth/register";
         }
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        ConfirmationCode code = new ConfirmationCode();
-        code.setCode(String.valueOf(new Random().nextLong(1234214)) +
-                (user.getEmail() + user.getPassword() + user.getUsername()).hashCode()
-        );
-        notificationService.sendEmailNotification(user.getEmail(), "Confirm Registration",
-                "Пожалуйста, используйте этот код для подтверждения регистрации: "  + code.getCode());
-        user = userRepository.save(user);
-        code.setUser(user);
-        confirmationCodeRepository.save(code);
+        user = userService.create(user);
         httpSession.setAttribute("email", user.getEmail());
-        return "redirect:auth/register/confirm";
+        return "redirect:/auth/register/confirm";
     }
 
     @GetMapping("/confirm")
     public String confirmPage(Model model, HttpSession session){
         String email = (String) session.getAttribute("email");
         model.addAttribute("email", email);
-        return "auth/confirm";
+        return "/auth/confirm";
     }
 
     @PostMapping("/confirm")
@@ -91,7 +65,7 @@ public class UserController {
         User user = confirmationCode.get().getUser();
         user.setEnabled(true);
         userRepository.save(user);
-        return "redirect:auth/login";
+        return "redirect:/auth/login";
     }
 }
 
--- src/main/java/ru/kalinichenko/softlink_logistic/service/UserService.java
+++ src/main/java/ru/kalinichenko/softlink_logistic/service/UserService.java
@@ -1,17 +1,33 @@
 package ru.kalinichenko.softlink_logistic.service;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import ru.kalinichenko.softlink_logistic.dao.ConfirmationCodeRepository;
 import ru.kalinichenko.softlink_logistic.dao.UserRepository;
 import ru.kalinichenko.softlink_logistic.entity.User;
+import ru.kalinichenko.softlink_logistic.entity.auth.ConfirmationCode;
+
+import java.util.Random;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final EmailNotificationService notificationService;
 
-    public UserService(UserRepository userRepository) {
+    private final ConfirmationCodeRepository confirmationCodeRepository;
+    public UserService(UserRepository userRepository,
+                       PasswordEncoder passwordEncoder,
+                       EmailNotificationService notificationService,
+                       ConfirmationCodeRepository confirmationCodeRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.notificationService = notificationService;
+        this.confirmationCodeRepository = confirmationCodeRepository;
     }
 
 
@@ -21,5 +37,36 @@ public class UserService {
 
     public User getAuthorized(){
         return getByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+
+    public boolean exists(User user){
+        return userRepository.exists(new Example<>() {
+            @Override
+            public User getProbe() {
+                return user;
+            }
+            @Override
+            public ExampleMatcher getMatcher() {
+                return ExampleMatcher.matchingAny()
+                        .withMatcher("email", ExampleMatcher.GenericPropertyMatcher::exact)
+                        .withMatcher("username", ExampleMatcher.GenericPropertyMatcher::exact);
+            }
+        });
+    }
+    public User create(User user){
+        if(exists(user)){
+            throw new IllegalArgumentException("Пользователь уже существует: проверьте имя или почту");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        ConfirmationCode code = new ConfirmationCode();
+        code.setCode(String.valueOf(new Random().nextLong(1234214)) +
+                (user.getEmail() + user.getPassword() + user.getUsername()).hashCode()
+        );
+        notificationService.sendEmailNotification(user.getEmail(), "Confirm Registration",
+                "Пожалуйста, используйте этот код для подтверждения регистрации: "  + code.getCode());
+        user = userRepository.save(user);
+        code.setUser(user);
+        confirmationCodeRepository.save(code);
+        return user;
     }
 }
