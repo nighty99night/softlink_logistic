c3af604d7c061603038ff1b932ac47c058036174
Maxim1720 <musiev17@gmail.com>
2023-06-21 00:28:20 +0400

    to change user id

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 INSERT INTO order_status (name) VALUES('Создан'),('В обработке'),('Выполняется'),('Завершен');
 insert into cargo_type (name, base_price) VALUES('Хрупкий', 500),('Стандартный', 250),('Опасный', 700);
-insert into users (id, username, password, email, enabled) values (1,'nikita','$2a$12$SBfOd.B4OfEuQzCZGNBDAeAgYNfWtEo0N5q2jWsRo16BofiD1bNNe', 'nikitanikita1799@gmail.com', true);
+insert into users (id, username, password, email, enabled) values (51,'nikita','$2a$12$SBfOd.B4OfEuQzCZGNBDAeAgYNfWtEo0N5q2jWsRo16BofiD1bNNe', 'nikitanikita1799@gmail.com', true);
