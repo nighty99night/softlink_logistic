b843ba5a65163040fb22dfc4410d878d387bea05
Maxim Musiev <musiev_maksim@mail.ru>
2023-06-21 13:55:36 +0400

    to add order statuses insertings in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,2 @@
+INSERT INTO order_status (name) VALUES('Создан'),('В обработке'),('Выполняется'),('Завершен');
 insert into cargo_type (name, base_price) VALUES('Хрупкий', 500),('Стандартный', 250),('Опасный', 700);
